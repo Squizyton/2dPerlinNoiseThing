9dbfa1fabd887d2a69ed40ed5a743230b17f1b7f
Joel <fallendemongod@gmail.com>
2023-05-29 21:19:40 -0400

    PlayerControls

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,9 +1,45 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Player
 {
     public class PlayerMovement : MonoBehaviour
     {
-        
+        private PlayerInput _controls;
+        private Vector2 movementDir;
+
+
+
+        void Start()
+        {
+            _controls = new PlayerInput();
+
+            _controls.Player.Movement.performed += OnMovement;
+            _controls.Enable();
+        }
+
+
+        void OnMovement(InputAction.CallbackContext ctx)
+        {
+            movementDir = _controls.Player.Movement.ReadValue<Vector2>();
+        }
+
+
+        private void FixedUpdate()
+        {
+            MoveCharacter();
+        }
+
+
+        void MoveCharacter()
+        {
+            if (movementDir.Equals(Vector2.zero)) return;
+            
+            
+            
+            
+            
+        }
     }
 }
